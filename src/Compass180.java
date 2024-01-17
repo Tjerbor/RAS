456cea12d7120fd10cab05fd275d88e47858705b
Thiemo Ottersbach <thiemo.ott@gmail.com>
2024-01-17 15:28:22 +0100

    Compass180

---
+++ src/Compass180.java
@@ -0,0 +1,7 @@
+import scripts.Compass;
+
+public class Compass180 {
+    public static void main(String[] args) throws InterruptedException {
+        Compass.main(null);
+    }
+}
--- src/TranferAll.bat
+++ src/TranferAll.bat
@@ -14,6 +14,22 @@ for /f "skip=3 tokens=2 delims= " %%a in ('tasklist /fi "imagename eq cmd.exe"')
 )
 del /f getCmdPID.exe %myprogram%.nxj %myprogram%.class
 
+set myprogram=Compass180
+Start nxjc %myprogram%.java
+PING localhost -n 2 >NUL
+Start nxjlink -o %myprogram%.nxj %myprogram%
+PING localhost -n 2 >NUL
+Start nxjupload %myprogram%.nxj
+PING localhost -n 5 >NUL
+call getCmdPID
+set "current_pid=%errorlevel%"
+for /f "skip=3 tokens=2 delims= " %%a in ('tasklist /fi "imagename eq cmd.exe"') do (
+    if "%%a" neq "%current_pid%" (
+        TASKKILL /PID %%a /f >nul 2>nul
+    )
+)
+del /f getCmdPID.exe %myprogram%.nxj %myprogram%.class
+
 set myprogram=FollowerJoscha
 Start nxjc %myprogram%.java
 PING localhost -n 2 >NUL
